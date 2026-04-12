f20a7fdfe2fd6b3ecf1d0213cbf219eabf8848c7
Alexandre Amorim de Jesus <151966815+Shyrriro@users.noreply.github.com>
2026-04-12 01:38:47 -0300

    feat: Add Email value object with format validation

---
+++ src/main/java/com/auth_service/core/domain/vo/Email.java
@@ -0,0 +1,44 @@
+package com.auth_service.core.domain.vo;
+
+import java.util.Objects;
+import java.util.regex.Pattern;
+
+public class Email {
+
+    private static final String EMAIL_REGEX =
+            "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+
+    private static final Pattern PATTERN = Pattern.compile(EMAIL_REGEX);
+
+    private final String value;
+
+    public Email(String value) {
+        validate(value);
+        this.value = value.toLowerCase(); // normaliza
+    }
+
+    private void validate(String value) {
+        if (value == null || value.isBlank()) {
+            throw new IllegalArgumentException("Campo Email não pode ser em branco");
+        }
+
+        if (!PATTERN.matcher(value).matches()) {
+            throw new IllegalArgumentException("Email inválido.");
+        }
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Email email)) return false;
+        return Objects.equals(value, email.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(value);
+    }
+}
