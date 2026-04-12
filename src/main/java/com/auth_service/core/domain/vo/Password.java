e59f96ea2aff3f86b2f27ec678f5f65fa8644767
Alexandre Amorim de Jesus <151966815+Shyrriro@users.noreply.github.com>
2026-04-12 01:38:56 -0300

    feat: Add Password value object with validation

---
+++ src/main/java/com/auth_service/core/domain/vo/Password.java
@@ -0,0 +1,45 @@
+package com.auth_service.core.domain.vo;
+
+import java.util.Objects;
+import java.util.regex.Pattern;
+
+public class Password {
+
+    private static final String PASSWORD_REGEX = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
+
+    private static final Pattern PATTERN = Pattern.compile(PASSWORD_REGEX);
+
+
+    private String value;
+
+    protected Password() {
+        // Construtor protegido para JPA
+    }
+
+    private void validate(String value) {
+        if (value == null || value.isBlank()) {
+            throw new IllegalArgumentException("Campo Password não pode ser em branco.");
+        }
+
+        if (!PATTERN.matcher(value).matches()) {
+            throw new IllegalArgumentException(
+                    "Password deve conter pelomenos 8 caracteres, incluindo letra maiúscula, minúscula, número, e caractere especial."
+            );
+        }
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Password password)) return false;
+        return Objects.equals(value, password.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(value);
+    }
+}
