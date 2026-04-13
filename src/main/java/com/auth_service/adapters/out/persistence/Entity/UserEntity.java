ad8b23bce49110d66fc780aa5e58510f029b0894
Alexandre Amorim de Jesus <151966815+Shyrriro@users.noreply.github.com>
2026-04-13 05:15:18 -0300

    feat: Add UserEntity

---
+++ src/main/java/com/auth_service/adapters/out/persistence/Entity/UserEntity.java
@@ -0,0 +1,40 @@
+package com.auth_service.adapters.out.persistence.Entity;
+
+import com.auth_service.core.domain.enums.Role;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false, length = 20)
+    private String username;
+
+    @Size(max = 100, message = "Email muito longo")
+    @Column(unique = true, nullable = false, length = 100)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+}
