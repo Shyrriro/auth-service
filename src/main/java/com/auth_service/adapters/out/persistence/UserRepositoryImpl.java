7aa240c3f5376c3b8d9f7555cf9e44e338c69867
Alexandre Amorim de Jesus <151966815+Shyrriro@users.noreply.github.com>
2026-04-14 03:16:58 -0300

    feat(persistence): refactor user saving logic and add methods to find and delete users by username and ID

--- src/main/java/com/auth_service/adapters/out/persistence/UserRepositoryImpl.java
+++ src/main/java/com/auth_service/adapters/out/persistence/UserRepositoryImpl.java
@@ -1,6 +1,6 @@
 package com.auth_service.adapters.out.persistence;
 
-import com.auth_service.adapters.out.persistence.Entity.UserEntity;
+import com.auth_service.adapters.out.persistence.entity.UserEntity;
 import com.auth_service.adapters.out.persistence.jpa.JpaUserRepository;
 import com.auth_service.core.domain.User;
 import com.auth_service.core.domain.enums.Role;
@@ -8,6 +8,9 @@ import com.auth_service.core.domain.ports.out.UserRepository;
 import com.auth_service.core.domain.vo.Email;
 import com.auth_service.core.domain.vo.Password;
 import com.auth_service.core.domain.vo.Username;
+
+import java.util.Optional;
+import java.util.UUID;
 
 public class UserRepositoryImpl implements UserRepository {
 
@@ -19,15 +22,23 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public User save(User user) {
-        UserEntity newUser = new UserEntity(
-                user.getId(),
-                user.getUsername().getValue(),
-                user.getEmail().getValue(),
-                user.getPassword().getValue(),
-                user.getRole().name()
-                );
 
-        var saved = jpaUserRepository.save(newUser);
+        UserEntity userEntity;
+
+        if (user.getId() != null) {
+            userEntity = jpaUserRepository.findById(user.getId())
+                    .orElseThrow();
+        } else {
+            userEntity = new UserEntity();
+        }
+
+        userEntity.setUsername(user.getUsername().getValue());
+        userEntity.setEmail(user.getEmail().getValue());
+        userEntity.setPassword(user.getPassword().getValue());
+        userEntity.setRole(user.getRole().name());
+
+        var saved = jpaUserRepository.save(userEntity);
+
         return new User(
                 saved.getId(),
                 new Username(saved.getUsername()),
@@ -38,12 +49,30 @@ public class UserRepositoryImpl implements UserRepository {
     }
 
     @Override
-    public boolean existsByUsername(Username username) {
-        return jpaUserRepository.existsByUsername(username.getValue());
+    public Optional<User> findUserByUsername(String username) {
+
+        return jpaUserRepository.findUserByUsername(username)
+                .map(userEntity -> new User(
+                        userEntity.getId(),
+                        new Username(userEntity.getUsername()),
+                        new Email(userEntity.getEmail()),
+                        new Password(userEntity.getPassword()),
+                        Role.valueOf(userEntity.getRole())
+                ));
     }
 
     @Override
-    public boolean existsByEmail(Email email) {
-        return jpaUserRepository.existsByEmail(email.getValue());
+    public boolean existsByUsername(String username) {
+        return jpaUserRepository.existsByUsername(username);
+    }
+
+    @Override
+    public boolean existsByEmail(String email) {
+        return jpaUserRepository.existsByEmail(email);
+    }
+
+    @Override
+    public void deleteById(UUID id) {
+        jpaUserRepository.deleteById(id);
     }
 }
