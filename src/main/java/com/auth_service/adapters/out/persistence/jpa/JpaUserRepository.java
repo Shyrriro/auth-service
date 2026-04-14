3a01250b887a6a0d3ad10cc05939f43127bcd305
Alexandre Amorim de Jesus <151966815+Shyrriro@users.noreply.github.com>
2026-04-14 03:16:05 -0300

    feat(persistence): add method to find user by username in JpaUserRepository

--- src/main/java/com/auth_service/adapters/out/persistence/jpa/JpaUserRepository.java
+++ src/main/java/com/auth_service/adapters/out/persistence/jpa/JpaUserRepository.java
@@ -1,11 +1,13 @@
 package com.auth_service.adapters.out.persistence.jpa;
 
-import com.auth_service.adapters.out.persistence.Entity.UserEntity;
+import com.auth_service.adapters.out.persistence.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface JpaUserRepository extends JpaRepository<UserEntity, UUID> {
     boolean existsByUsername(String value);
     boolean existsByEmail(String email);
+    Optional<UserEntity> findUserByUsername(String username);
 }
