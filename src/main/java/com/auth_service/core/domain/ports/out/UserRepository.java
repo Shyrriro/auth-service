e1bfbbfca6b27853ddc1dfab2f2ff9a5015970f9
Alexandre Amorim de Jesus <151966815+Shyrriro@users.noreply.github.com>
2026-04-14 03:16:19 -0300

    feat(persistence): add methods to find and check existence of users by username and email

--- src/main/java/com/auth_service/core/domain/ports/out/UserRepository.java
+++ src/main/java/com/auth_service/core/domain/ports/out/UserRepository.java
@@ -1,7 +1,20 @@
 package com.auth_service.core.domain.ports.out;
 
 import com.auth_service.core.domain.User;
+import com.auth_service.core.domain.vo.Email;
+
+import java.util.Optional;
+import java.util.UUID;
 
 public interface UserRepository {
+
     User save(User user);
+
+    Optional<User> findUserByUsername(String username);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
+
+    void deleteById(UUID id);
 }
