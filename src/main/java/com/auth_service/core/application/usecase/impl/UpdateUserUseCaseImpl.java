7339fa574aeec572a9714fbb82144d38ac18d175
Alexandre Amorim de Jesus <151966815+Shyrriro@users.noreply.github.com>
2026-04-14 03:23:08 -0300

    feat(usecase): implement UpdateUserUseCaseImpl for updating user details

---
+++ src/main/java/com/auth_service/core/application/usecase/impl/UpdateUserUseCaseImpl.java
@@ -0,0 +1,50 @@
+package com.auth_service.core.application.usecase.impl;
+
+import com.auth_service.adapters.in.web.dto.request.UserUpdateDTO;
+import com.auth_service.adapters.in.web.dto.response.UserResponseDTO;
+import com.auth_service.core.application.usecase.UpdateUserUseCase;
+import com.auth_service.core.domain.User;
+import com.auth_service.core.domain.ports.out.UserRepository;
+import com.auth_service.core.domain.vo.Email;
+import com.auth_service.core.domain.vo.Password;
+import com.auth_service.core.domain.vo.Username;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class UpdateUserUseCaseImpl implements UpdateUserUseCase {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder encoder;
+
+    public UpdateUserUseCaseImpl(UserRepository userRepository, BCryptPasswordEncoder encoder) {
+        this.userRepository = userRepository;
+        this.encoder = encoder;
+    }
+
+    @Override
+    public UserResponseDTO execute(String username, UserUpdateDTO userUpdateDTO) {
+
+        User user = userRepository.findUserByUsername(username)
+                .orElseThrow(() -> new DataIntegrityViolationException("Usuário não encontrado")
+        );
+
+        if (userUpdateDTO.username() != null && !userUpdateDTO.username().isBlank()) {
+            user = user.updateUsername(new Username(userUpdateDTO.username()));
+        }
+        if (userUpdateDTO.email() != null && !userUpdateDTO.email().isBlank()){
+            user = user.updateEmail(new Email(userUpdateDTO.email()));
+        }
+        if (userUpdateDTO.password() != null && !userUpdateDTO.password().isBlank()) {
+            user = user.changePassword(Password.create(userUpdateDTO.password(), encoder));
+        }
+
+        userRepository.save(user);
+
+        return new UserResponseDTO(
+                    user.getId(),
+                    user.getUsername().getValue(),
+                    user.getEmail().getValue(),
+                    user.getRole().name()
+        );
+    }
+}
