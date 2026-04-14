437f7add4d45ff582d102373710b1217b01e0583
Alexandre Amorim de Jesus <151966815+Shyrriro@users.noreply.github.com>
2026-04-14 03:17:53 -0300

    feat(validation): update email validation rules for length and format in UserRequestDTO

--- src/main/java/com/auth_service/adapters/in/web/dto/request/UserRequestDTO.java
+++ src/main/java/com/auth_service/adapters/in/web/dto/request/UserRequestDTO.java
@@ -8,11 +8,11 @@ public record UserRequestDTO(
         @Size(min = 3, max = 20, message = "O nome de usuário deve ter entre 3 e 20 caracteres.")
         @NotBlank(message = "Nome de usuário é obrigatório")
         String username,
-        @Email(message = "Email inválido", regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
+        @Size(min = 6, max = 254, message = "O email deve ter entre 6 e 254 caracteres.")
+        @Email(message = "Email inválido", regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$")
         @NotBlank(message = "Email é obrigatório")
         String email,
         @Size(min = 8, message = "A senha deve ter pelo menos 8 caracteres.")
         @NotBlank(message = "Password é obrigatório")
-        String password,
-        String role) {
+        String password) {
 }
