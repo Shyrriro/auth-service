33bbda3a15349705afd04df690e882acd90b222b
Alexandre Amorim de Jesus <151966815+Shyrriro@users.noreply.github.com>
2026-04-21 02:03:15 -0300

    feat(security): implement AuthorizationService for user authentication

---
+++ src/main/java/com/auth_service/config/security/AuthorizationService.java
@@ -0,0 +1,22 @@
+package com.auth_service.config.security;
+
+import com.auth_service.core.domain.ports.out.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AuthorizationService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = userRepository.findUserByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado com username: " + username));
+        return new CustomUserDetails(user);
+    }
+}
