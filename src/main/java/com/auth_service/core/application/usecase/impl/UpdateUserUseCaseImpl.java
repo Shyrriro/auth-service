bb496a0347562b010a6ecaf507ab3a1847a15c1d
Alexandre Amorim de Jesus <151966815+Shyrriro@users.noreply.github.com>
2026-04-21 02:00:03 -0300

    feat(user): update UpdateUserUseCaseImpl to use PasswordHasher for password changes and add password confirmation check

--- src/main/java/com/auth_service/core/application/usecase/impl/UpdateUserUseCaseImpl.java
+++ src/main/java/com/auth_service/core/application/usecase/impl/UpdateUserUseCaseImpl.java
@@ -4,28 +4,28 @@ import com.auth_service.adapters.in.web.dto.request.UserUpdateDTO;
 import com.auth_service.adapters.in.web.dto.response.UserResponseDTO;
 import com.auth_service.core.application.usecase.UpdateUserUseCase;
 import com.auth_service.core.domain.User;
+import com.auth_service.core.domain.ports.out.PasswordHasher;
 import com.auth_service.core.domain.ports.out.UserRepository;
 import com.auth_service.core.domain.vo.Email;
 import com.auth_service.core.domain.vo.Password;
 import com.auth_service.core.domain.vo.Username;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class UpdateUserUseCaseImpl implements UpdateUserUseCase {
 
     private final UserRepository userRepository;
-    private final PasswordEncoder encoder;
+    private final PasswordHasher passwordHasher;
 
-    public UpdateUserUseCaseImpl(UserRepository userRepository, PasswordEncoder encoder) {
+    public UpdateUserUseCaseImpl(UserRepository userRepository, PasswordHasher passwordHasher) {
         this.userRepository = userRepository;
-        this.encoder = encoder;
+        this.passwordHasher = passwordHasher;
     }
 
     @Override
     public UserResponseDTO execute(String username, UserUpdateDTO userUpdateDTO) {
 
         User user = userRepository.findUserByUsername(username)
-                .orElseThrow(() -> new DataIntegrityViolationException("Usuário não encontrado")
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado")
         );
 
         if (userUpdateDTO.username() != null && !userUpdateDTO.username().isBlank()) {
@@ -34,8 +34,12 @@ public class UpdateUserUseCaseImpl implements UpdateUserUseCase {
         if (userUpdateDTO.email() != null && !userUpdateDTO.email().isBlank()){
             user = user.updateEmail(new Email(userUpdateDTO.email()));
         }
-        if (userUpdateDTO.password() != null && !userUpdateDTO.password().isBlank()) {
-            user = user.changePassword(Password.create(userUpdateDTO.password(), encoder));
+        if (userUpdateDTO.newPassword() != null && !userUpdateDTO.newPassword().isBlank() && userUpdateDTO.passwordConfirm() != null && !userUpdateDTO.passwordConfirm().isBlank()) {
+            if(userUpdateDTO.newPassword().equals(userUpdateDTO.passwordConfirm())){
+            user = user.changePassword(Password.create(userUpdateDTO.newPassword(), passwordHasher));
+            } else {
+                throw new IllegalArgumentException("Senhas não coincidem.");
+            }
         }
 
         userRepository.save(user);
