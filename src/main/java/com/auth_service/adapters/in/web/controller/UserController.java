cb55885a87b6e15e39a4e88fa2f75cab8e0ce10f
Alexandre Amorim de Jesus <151966815+Shyrriro@users.noreply.github.com>
2026-04-21 02:04:50 -0300

    feat(user): update UserController with validation and authorization for user operations

--- src/main/java/com/auth_service/adapters/in/web/controller/UserController.java
+++ src/main/java/com/auth_service/adapters/in/web/controller/UserController.java
@@ -6,12 +6,14 @@ import com.auth_service.core.application.usecase.impl.CreateUserUseCaseImpl;
 import com.auth_service.core.application.usecase.impl.DeleteUserUseCaseImpl;
 import com.auth_service.core.application.usecase.impl.GetUserByUsernameUseCaseImpl;
 import com.auth_service.core.application.usecase.impl.UpdateUserUseCaseImpl;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping()
 public class UserController {
 
     private final CreateUserUseCaseImpl createUser;
@@ -26,24 +28,25 @@ public class UserController {
         this.deleteUser = deleteUser;
     }
 
-
-    @PostMapping
-    public ResponseEntity<?> createUser(@RequestBody UserRequestDTO userRequestDTO) {
+    @PostMapping("auth/singup")
+    public ResponseEntity<?> createUser(@RequestBody @Valid UserRequestDTO userRequestDTO) {
             return ResponseEntity.status(HttpStatus.CREATED).body(createUser.execute(userRequestDTO));
     }
 
-    @GetMapping("/{username}")
-    private ResponseEntity<?> getUser(@PathVariable String username){
+    @GetMapping("/search/{username}")
+    public ResponseEntity<?> getUser(@PathVariable String username){
         return ResponseEntity.status(HttpStatus.OK).body(getUserByUsername.execute(username));
     }
 
-    @PutMapping("/{username}")
-    public ResponseEntity<?> updateUser(@PathVariable String username, @RequestBody UserUpdateDTO userUpdateDTO) {
+    @PreAuthorize("#username == authentication.name")
+    @PutMapping("/updateaccount/{username}")
+    public ResponseEntity<?> updateUser(@PathVariable @Valid String username, @RequestBody UserUpdateDTO userUpdateDTO) {
             return ResponseEntity.status(HttpStatus.OK).body(updateUser.execute(username, userUpdateDTO));
     }
 
-    @DeleteMapping("/{username}")
-    public ResponseEntity<?> deleteUser(@PathVariable String username) {
+    @PreAuthorize("#username == authentication.name")
+    @DeleteMapping("/deleteaccount/{username}")
+    public ResponseEntity<?> deleteUser(@PathVariable @Valid String username) {
         return ResponseEntity.status(HttpStatus.OK).body(deleteUser.execute(username));
     }
 }
