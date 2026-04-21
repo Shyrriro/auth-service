84bdbc6884cd6ab243c4b323dd5e6b99bb85219c
Alexandre Amorim de Jesus <151966815+Shyrriro@users.noreply.github.com>
2026-04-21 02:00:21 -0300

    feat(user): update CreateUserUseCaseImpl to use PasswordHasher and add password confirmation check

--- src/main/java/com/auth_service/core/application/usecase/impl/CreateUserUseCaseImpl.java
+++ src/main/java/com/auth_service/core/application/usecase/impl/CreateUserUseCaseImpl.java
@@ -6,48 +6,51 @@ import com.auth_service.config.exceptions.UserAlreadyExistsException;
 import com.auth_service.core.domain.User;
 import com.auth_service.core.application.usecase.CreateUserUseCase;
 import com.auth_service.core.domain.enums.Role;
+import com.auth_service.core.domain.ports.out.PasswordHasher;
 import com.auth_service.core.domain.ports.out.UserRepository;
 import com.auth_service.core.domain.vo.Email;
 import com.auth_service.core.domain.vo.Password;
 import com.auth_service.core.domain.vo.Username;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class CreateUserUseCaseImpl implements CreateUserUseCase {
 
     private final UserRepository userRepository;
-    private PasswordEncoder passwordEncoder;
+    private final PasswordHasher passwordHasher;
 
-    public CreateUserUseCaseImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public CreateUserUseCaseImpl(UserRepository userRepository, PasswordHasher passwordHasher) {
         this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
+        this.passwordHasher = passwordHasher;
     }
 
     @Override
     public UserResponseDTO execute(UserRequestDTO userRequestDTO) {
 
-        Password password = Password.create(userRequestDTO.password(), passwordEncoder);
+        if (userRequestDTO.password().equals(userRequestDTO.passwordConfirm())){
+            Password password = Password.create(userRequestDTO.password(), passwordHasher);
+            User user = new User(
+                    null,
+                    new Username(userRequestDTO.username()),
+                    new Email(userRequestDTO.email()),
+                    password,
+                    Role.USER
+            );
 
-        User user = new User(
-                null,
-                new Username(userRequestDTO.username()),
-                new Email(userRequestDTO.email()),
-                password,
-                Role.USER
-        );
+            if(userRepository.existsByUsername(user.getUsername().getValue())) {
+                throw new UserAlreadyExistsException("nome", userRequestDTO.username());
+            } else if (userRepository.existsByEmail(user.getEmail().getValue())) {
+                throw new UserAlreadyExistsException("email", userRequestDTO.email());
+            }
 
-        if(userRepository.existsByUsername(user.getUsername().getValue())) {
-            throw new UserAlreadyExistsException("nome", userRequestDTO.username());
-        } else if (userRepository.existsByEmail(user.getEmail().getValue())) {
-            throw new UserAlreadyExistsException("email", userRequestDTO.email());
+            var saved = userRepository.save(user);
+
+            return new UserResponseDTO(
+                    saved.getId(),
+                    saved.getUsername().getValue(),
+                    saved.getEmail().getValue(),
+                    saved.getRole().name()
+            );
+        } else {
+            throw new IllegalArgumentException("Senhas não coincidem");
         }
-
-        var saved = userRepository.save(user);
-
-        return new UserResponseDTO(
-                saved.getId(),
-                saved.getUsername().getValue(),
-                saved.getEmail().getValue(),
-                saved.getRole().name()
-        );
     }
 }
