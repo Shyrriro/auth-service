865dc5450a10e90024f14b957999b741c450841f
Alexandre Amorim de Jesus <151966815+Shyrriro@users.noreply.github.com>
2026-04-22 11:12:30 -0300

    fix(user): improve user deletion logic to throw exception if user not found

--- src/main/java/com/auth_service/core/application/usecase/impl/DeleteUserUseCaseImpl.java
+++ src/main/java/com/auth_service/core/application/usecase/impl/DeleteUserUseCaseImpl.java
@@ -4,9 +4,9 @@ import com.auth_service.adapters.in.web.dto.response.ResponseDTO;
 import com.auth_service.core.application.usecase.DeleteUserUseCase;
 import com.auth_service.core.domain.User;
 import com.auth_service.core.domain.ports.out.UserRepository;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
 
 public class DeleteUserUseCaseImpl implements DeleteUserUseCase {
 
@@ -19,9 +19,11 @@ public class DeleteUserUseCaseImpl implements DeleteUserUseCase {
     @Override
     public ResponseDTO execute(String username) {
 
-        Optional<User> user = userRepository.findUserByUsername(username);
+        User user = userRepository.findUserByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado")
+        );
 
-        userRepository.deleteById(user.get().getId());
+        userRepository.deleteById(user.getId());
 
         return new ResponseDTO(
                 200,
